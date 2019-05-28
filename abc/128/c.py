2edc67ccb6ada85f735f88e701e846d5202a4fb5
d6ms <g.denden.ms@gmail.com>
2019-05-28 22:44:08 +0900

    bit zentansaku

--- abc/128/c.py
+++ abc/128/c.py
@@ -1,15 +1,16 @@
+# コンテスト中は無理やりなビット全探索[alias bit全探索]をしたが、正しく書き換えた
+
 N, M = map(int, input().split())
 S = [list(map(int, input().split()))[1:] for _ in range(M)]
 P = list(map(int, input().split()))
 
 count = 0
 for i in range(2 ** N):
-    state = ''.join(list(reversed(str(bin(i))[2:].zfill(N))))
     flg = True
     for m in range(M):
         on = 0
         for s in S[m]:
-            if state[s - 1] == '1':
+            if (i >> s - 1) & 1:
                 on += 1
         if on % 2 != P[m]:
             flg = False
