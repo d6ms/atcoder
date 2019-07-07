307ec76b81ad71ae765702de045a1a55f7b008c5
d6ms <g.denden.ms@gmail.com>
2019-07-07 23:54:09 +0900

    abc 133 解き直し

--- abc/133/d.py
+++ abc/133/d.py
@@ -1,14 +1,15 @@
-# コンテスト中ではTLE
-
 N = int(input())
 A = list(map(int, input().split()))
 
+# x1さえ求めてしまえば連立方程式の解は連鎖的にO(N)で求まる
 X = list()
+x_1 = sum(A)
 for i in range(N):
-    x_i = A[i]
-    keisu = -1
-    for j in range(1, N):
-        x_i += keisu * A[(i + j) % N]
-        keisu = keisu * -1
-    X.append(str(x_i))
-print(' '.join(X))
+    if i % 2 != 0:
+        x_1 -= 2 * A[i]
+X.append(x_1)
+
+for i in range(1, N):
+    x_i = 2 * A[i - 1] - X[i - 1]
+    X.append(x_i)
+print(' '.join([str(x) for x in X]))
