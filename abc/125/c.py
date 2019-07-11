9955ba05fcba74cbc3504cff2cf2f5836040c6db
d6ms <g.denden.ms@gmail.com>
2019-07-11 18:51:49 +0900

    abc 125 c 解き直し

--- abc/125/c.py
+++ abc/125/c.py
@@ -1,28 +1,27 @@
-import fractions
-
-
-def solve(N, A):
-    max_gcd = 0
-    gcd1memo = [None] * N  # gcd [0, i]
-    gcd2memo = [None] * N  # gcd [i, N - 1]
-    gcd1memo[0] = A[0]
-    gcd2memo[N - 1] = A[N - 1]
-    for i in range(1, N):
-        gcd1memo[i] = fractions.gcd(gcd1memo[i - 1], A[i])
-    for i in reversed(range(N - 1)):
-        gcd2memo[i] = fractions.gcd(gcd2memo[i + 1], A[i])
-    for i in range(N):
-        gcd = None
-        if i == 0:
-            gcd = gcd2memo[1]
-        elif i == N - 1:
-            gcd = gcd1memo[N - 1]
-        else:
-            gcd = fractions.gcd(gcd1memo[i - 1], gcd2memo[i + 1])
-        max_gcd = max(max_gcd, gcd)
-    return max_gcd
+from fractions import gcd
 
 
 N = int(input())
 A = list(map(int, input().split()))
-print(solve(N, A))
+
+# l[i] := [0, i]の数のGCDメモ
+l = [None for _ in range(N)]
+l[0] = A[0]
+for i in range(1, N):
+    l[i] = gcd(l[i - 1], A[i])
+
+# r[i] := [i, N - 1]の数のGCDメモ
+r = [None for _ in range(N)]
+r[N - 1] = A[N - 1]
+for i in reversed(range(N - 1)):
+    r[i] = gcd(r[i + 1], A[i])
+
+ans = 0
+for i in range(N):
+    if i == 0:
+        ans = max(ans, r[1])
+    elif i == N - 1:
+        ans = max(ans, l[N - 2])
+    else:
+        ans = max(ans, gcd(l[i - 1], r[i + 1]))
+print(ans)
