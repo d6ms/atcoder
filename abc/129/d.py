c6bf1ba7f5644d91d024d7ca6ae1afdd5c5cbc12
d6ms <g.denden.ms@gmail.com>
2019-07-11 20:55:02 +0900

    ab 129 d 解き直し

--- abc/129/d.py
+++ abc/129/d.py
@@ -1,1 +1,20 @@
+import numpy as np
+
+
 H, W = map(int, input().split())
+grid = np.array([list(input()) for _ in range(H)]) == "."
+
+up = np.zeros((H, W), dtype = int)
+down = np.zeros((H, W), dtype = int)
+left = np.zeros((H, W), dtype = int)
+right = np.zeros((H, W), dtype = int)
+
+for i in range(H):
+    down[i] = (down[i - 1] + 1) * grid[i]
+    up[-i - 1] = (up[-i] + 1) * grid[-i - 1]
+
+for i in range(W):
+    right[:, i] = (right[:, i - 1] + 1) * grid[:, i]
+    left[:, -i - 1] = (left[:, -i] + 1) * grid[:, -i - 1]
+
+print(np.max(up + down + left + right) - 3)
