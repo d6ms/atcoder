71211ec566ab2f6e4b5dd5632e8c8a32a1ff6bda
d6ms <g.denden.ms@gmail.com>
2019-08-17 20:56:59 +0900

    abc 087

--- abc/087/d.py
+++ abc/087/d.py
@@ -1,8 +1,30 @@
-def solve():
-    pass
-
+import sys
+sys.setrecursionlimit(300000)
 
 N, M = map(int, input().split())
-L_R_D = [tuple(map(int, input().split())) for _ in range(M)]
+nl = [[] for _ in range(N + 1)]
+for _ in range(M):
+    L, R, D = map(int, input().split())
+    nl[L].append((R, D))
+    nl[R].append((L, -D))
 
-print(solve())
+seen = [None for _ in range(N + 1)]
+
+
+def dfs(v, d):
+    seen[v] = d
+    for next_v, D in nl[v]:
+        next_d = d + D
+        if seen[next_v] is None:
+            dfs(next_v, next_d)
+        else:
+            if seen[next_v] != next_d:
+                print('No')
+                exit(0)
+
+
+for v in range(1, N + 1):  # すべての連結成分に対してDFS
+    if seen[v] is None:
+        dfs(v, 0)
+
+print('Yes')
