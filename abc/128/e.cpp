244dad9fe354da0551050acb0dfe89c06b196878
d6ms <g.denden.ms@gmail.com>
2019-08-30 02:37:15 +0900

    test commit for username change

---
+++ abc/128/e.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, t) for (int i = (s); i < (int)(t); i++)
+
+int main() {
+    int N, Q;
+    cin >> N >> Q;
+
+    priority_queue<int, vector<int>, greater<int>> event;
+}
