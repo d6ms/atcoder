9e0efe86eff28e050ea8353dae87bb7692434021
d6ms <g.denden.ms@gmail.com>
2019-10-30 19:08:41 +0900

    abc 121 d

--- py/abc/121/d.py
+++ py/abc/121/d.py
@@ -1,7 +1,22 @@
+from operator import xor
 
 A, B = map(int, input().split())
-x = A
-for i in range(A + 1, B + 1):
-    x = x ^ i
 
-print(x)
+
+def f(n):
+    if n % 2 == 0:
+        if n % 4 == 0:
+            return n
+        else:
+            return xor(n, 1)
+    else:
+        return xor(n, f(n - 1))
+
+
+print(xor(f(A - 1), f(B)))
+
+# 0,   1,   2,   3,   4,   5,   6,   7
+# 000, 001, 010, 011, 100, 101, 110, 111
+# 000, 001, 011, 000, 100, 001, 000, 111
+# f(A, B) = f(0, A - 1) ^ f(0, B)
+# f(0, X) = X ^ (X - 1)
