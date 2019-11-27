48e49a248bf5e54352d850b956e28f1ecfe38b77
d6ms <g.denden.ms@gmail.com>
2019-11-27 19:31:19 +0900

    abc 138 e 小手先の高速化

--- py/abc/138/e.py
+++ py/abc/138/e.py
@@ -1,33 +1,42 @@
+# 小手先の高速化してみたけどTLE
 from collections import defaultdict
+import sys
+input = sys.stdin.readline
 
-s = input()
-t = input()
 
-charset = set()
-for c in t:
-    charset.add(c)
-for c in charset:
-    if c not in s:
-        print(-1)
-        exit(0)
+def main():
+    s = input().strip()
+    t = input().strip()
 
-d = defaultdict(list)  # d[c] := cはsの何番目に出現するか
-for i, c in enumerate(s):
-    d[c].append(i + 1)
+    charset = set()
+    for c in t:
+        charset.add(c)
+    for c in charset:
+        if c not in s:
+            print(-1)
+            exit(0)
 
-ans = 0
-S = s
-at = 0
-for c in t:
-    found = False
-    while not found:
-        for i in d[c]:
-            if i > at:
-                ans += i - at
-                at = i
-                found = True
-                break
-        if not found:
-            ans += len(S) - at
-            at = 0
-print(ans)
+    d = defaultdict(list)  # d[c] := cはsの何番目に出現するか
+    for i, c in enumerate(s):
+        d[c].append(i + 1)
+
+    ans = 0
+    S = s
+    at = 0
+    for c in t:
+        found = False
+        while not found:
+            for i in d[c]:
+                if i > at:
+                    ans += i - at
+                    at = i
+                    found = True
+                    break
+            if not found:
+                ans += len(S) - at
+                at = 0
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
