158dfec95d986b5777bdc440e3f66cbe5cf1b43c
d6ms <g.denden.ms@gmail.com>
2020-01-11 19:21:49 +0900

    abc 150 d 解き直し

--- py/abc/150/d.py
+++ py/abc/150/d.py
@@ -7,14 +7,26 @@ def main():
     N, M = map(int, input().split())
     A = list(map(lambda x: int(x) // 2, input().split()))
 
-    v = A[0]
+    X = A[0]
     for i in range(1, N):
-        a, b = v, A[i]
-        v = a * b // gcd(a, b)
-        if v > M:
+        a, b = X, A[i]
+        X = a * b // gcd(a, b)
+        if X > M:
             print(0)
             exit(0)
-    print((M // v + 1) // 2)
+
+    cnt = 0
+    tmp = X
+    while tmp % 2 == 0:
+        tmp = tmp // 2
+        cnt += 1
+    for a in A:
+        if a % (2 ** cnt) == 0 and a % (2 ** (cnt + 1)) != 0:
+            continue
+        else:
+            print(0)
+            exit(0)
+    print((M // X + 1) // 2)
 
 
 if __name__ == '__main__':
