b5cb84a0cc28d625e58716fa2131054dab65eea7
d6ms <g.denden.ms@gmail.com>
2020-01-13 23:17:52 +0900

    abc 151 e

--- py/abc/151/e.py
+++ py/abc/151/e.py
@@ -1,17 +1,32 @@
+
+class BinomialCoefficient(object):
+
+    def __init__(self, N=510000, MOD=10**9+7):
+        self.fac = [1, 1]
+        self.finv = [1, 1]
+        self.inv = [0, 1]
+        self.MOD = MOD
+        for i in range(2, N + 1):
+            self.fac.append((self.fac[-1] * i) % MOD)
+            self.inv.append((-self.inv[MOD % i] * (MOD // i)) % MOD)
+            self.finv.append((self.finv[-1] * self.inv[-1]) % MOD)
+
+    def comb(self, n, r):
+        if r < 0 or n < r or r < 0:
+            return 0
+        return self.fac[n] * self.finv[r] * self.finv[n - r] % self.MOD
+
+
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 MOD = 10 ** 9 + 7
 
-if K == 1:
-    print(0)
-    exit(0)
-
 A.sort()
-s = 0
-for i in range(N):
-    plus_times = max(0, i - K + 2)
-    minus_times = 0 if i + K > N else N - i - 1
-    s = s + (plus_times * A[i])
-    s = s - (minus_times * A[i])
-    s %= MOD
-print(s)
+bc = BinomialCoefficient(N)
+nCk = bc.comb(N, K)
+ans = 0
+for i in range(N - 1):
+    c = nCk - bc.comb(i + 1, K) - bc.comb((N - i - 1), K)
+    ans += c * (A[i + 1] - A[i]) % MOD
+    ans %= MOD
+print(ans)
--- py/lib/number.py
+++ py/lib/number.py
@@ -104,6 +104,24 @@ def create_factorial(n):
     return f
 
 
+class BinomialCoefficient(object):
+
+    def __init__(self, N=510000, MOD=10**9+7):
+        self.fac = [1, 1]
+        self.finv = [1, 1]
+        self.inv = [0, 1]
+        self.MOD = MOD
+        for i in range(2, N + 1):
+            self.fac.append((self.fac[-1] * i) % MOD)
+            self.inv.append((-self.inv[MOD % i] * (MOD // i)) % MOD)
+            self.finv.append((self.finv[-1] * self.inv[-1]) % MOD)
+
+    def comb(self, n, r):
+        if r < 0 or n < r or r < 0:
+            return 0
+        return self.fac[n] * self.finv[r] * self.finv[n - r] % self.MOD
+
+
 from fractions import gcd
 
 def lcm(a, b):
