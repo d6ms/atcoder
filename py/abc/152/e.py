27018820ccb7e804524f4e7433af2e345113ecb2
d6ms <g.denden.ms@gmail.com>
2020-01-22 22:16:50 +0900

    abc 152 e 逆元計算を正しく行って高速化

--- py/abc/152/e.py
+++ py/abc/152/e.py
@@ -1,18 +1,23 @@
 from fractions import gcd
+from functools import reduce
 import sys
+
 input = sys.stdin.readline
 MOD = 10 ** 9 + 7
+
+
+def inverse(n, mod=10 ** 9 + 7):
+    return pow(n, mod - 2, mod)
 
 
 def main():
     N = int(input())
     A = list(map(int, input().split()))
 
-    l = A[0]
-    for i in range(1, N):
-        l = l * A[i] // gcd(l, A[i])
+    L = reduce(lambda o1, o2: o1 // gcd(o1, o2) * o2, A)
+    sigma_A_inv = sum([inverse(a) for a in A])
 
-    print(sum([l // a for a in A]) % MOD)
+    print(L * sigma_A_inv % MOD)
 
 
 if __name__ == '__main__':
