4601d680e4388099e01857b8d3bb9f38cc89fe9c
d6ms <g.denden.ms@gmail.com>
2020-01-28 19:24:02 +0900

    abc 153 e

--- py/abc/153/e.py
+++ py/abc/153/e.py
@@ -1,31 +1,15 @@
-from fractions import Fraction
-
 H, N = map(int, input().split())
-AB = [tuple(map(int, input().split())) for _ in range(N)]
 
-ma, mb = None, None
-m_cospa = 0
-for a, b in AB:
-    cospa = Fraction(a, b)
-    if cospa > m_cospa:
-        ma, mb = a, b
-        m_cospa = cospa
+# dp[i][j] = 魔法iまでからダメージの総和がj以上になるように選んだ時の魔力の最小値
+# dp[i][j] = min(dp[i][j - a] + b, dp[i - 1][j])
 
-ans = H // ma * mb
-H %= ma
-
-if H == 0:
-    print(ans)
-    exit(0)
-
-min_pow = float('inf')
-for a, b in AB:
-    if a >= H:
-        mp = b
-    elif H % a == 0:
-        mp = H // a * b
-    else:
-        mp = (H // a + 1) * b
-    min_pow = min(min_pow, mp)
-ans += min_pow
-print(ans)
+# この形式の初期化はTLEする
+# dp = [[float('inf') for _ in range(H + 1)] for _ in range(N + 1)]
+dp = [[float('inf')] * (H + 1)] * (N + 1)
+for i in range(N + 1):
+    dp[i][0] = 0
+for i in range(1, N + 1):
+    a, b = map(int, input().split())
+    for j in range(H + 1):
+        dp[i][j] = min(dp[i][max(0, j - a)] + b, dp[i - 1][j])
+print(dp[N][H])
