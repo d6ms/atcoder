addabb83d5517b3a113ef8182b788c37163eb567
d6ms <g.denden.ms@gmail.com>
2020-02-06 18:58:08 +0900

    abc 120 d

--- py/abc/120/d.py
+++ py/abc/120/d.py
@@ -1,11 +1,10 @@
-# UnionFindの利用とreverseの発想まではOKだがTLE
-
-
 class UnionFind(object):
 
     def __init__(self, N):
         self._tree = [i for i in range(N)]
         self._rank = [0] * (N + 1)
+        self.size = [1] * N
+        self.inconvenience = N * (N - 1) // 2
 
     def find(self, x):
         if self._tree[x] == x:
@@ -18,38 +17,28 @@ class UnionFind(object):
         x = self.find(x)
         y = self.find(y)
 
-        # ランクを考慮しない場合はどちらが親になっても良い
+        if x == y:
+            return
+
+        self.inconvenience -= self.size[x] * self.size[y]
         if self._rank[x] < self._rank[y]:
             self._tree[x] = y
+            self.size[y] += self.size[x]
         else:
             self._tree[y] = x
+            self.size[x] += self.size[y]
 
         if self._rank[x] == self._rank[y]:
             self._rank[x] += 1
-
-    def has_same_parent(self, x, y):
-        return self.find(x) == self.find(y)
 
 
 N, M = map(int, input().split())
 A_B = [tuple(map(int, input().split())) for _ in range(M)]
 
-
-def calc_inconvenience(unionfind, N):
-    inconvenience = 0
-    for i in range(1, N + 1):
-        for j in range(i + 1, N + 1):
-            if not unionfind.has_same_parent(i, j):
-                inconvenience += 1
-    return inconvenience
-
-
 result = list()
-unionfind = UnionFind(N + 1)
+uf = UnionFind(N)
 for a, b in reversed(A_B):
-    result.append(calc_inconvenience(unionfind, N))
-    unionfind.unite(a, b)
+    result.append(max(0, uf.inconvenience))
+    uf.unite(a - 1, b - 1)
 for r in reversed(result):
     print(r)
-
-
--- py/lib/union_find.py
+++ py/lib/union_find.py
@@ -3,6 +3,7 @@ class UnionFind(object):
     def __init__(self, N):
         self.tree = [i for i in range(N)]
         self.rank = [0] * (N + 1)
+        self.size = [1] * N  # 頂点iの属する連結成分の大きさ
 
     def find(self, x):
         if self.tree[x] == x:
@@ -14,12 +15,16 @@ class UnionFind(object):
     def unite(self, x, y):
         x = self.find(x)
         y = self.find(y)
+        if x == y:
+            return
 
         # ランクを考慮しない場合はどちらが親になっても良い
         if self.rank[x] < self.rank[y]:
             self.tree[x] = y
+            self.size[y] += self.size[x]
         else:
             self.tree[y] = x
+            self.size[x] += self.size[y]
 
         if self.rank[x] == self.rank[y]:
             self.rank[x] += 1
