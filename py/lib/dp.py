70bbdad8ef4e590e55411b0a0c6ea24c8175ddcd
d6ms <g.denden.ms@gmail.com>
2020-02-11 20:11:16 +0900

    digit dp

--- py/lib/dp.py
+++ py/lib/dp.py
@@ -84,3 +84,26 @@ def partial_sum_count():
                 continue
             dp[i][j] = (dp[i - 1][j - a[i]] + dp[i - 1][j]) % MOD
     print(dp[N][A])
+
+
+# 桁DP
+# N以下でいずれかの桁に3が現れる数字の数を数える
+# https://qiita.com/pinokions009/items/1e98252718eeeeb5c9ab
+def digit_dp(N):
+    l = len(N)
+
+    # dp[i][smaller][three] := i桁目までで3が現れた(three)数字の数
+    dp = [[[0] * 2 for _ in range(2)] for _ in range(l + 1)]
+    dp[0][False][False] = 1
+
+    for i in range(1, l + 1):
+        N_i = int(N[i - 1])
+        for smaller in [True, False]:
+            for three in [True, False]:
+                for d in range((9 if smaller else N_i) + 1):
+                    # 各フラグに寄与する条件を左辺に書く
+                    # e.g. smaller は既にN未満である場合か、現在桁でN未満であることが確定した場合
+                    dp[i][smaller or d < N_i][three or d == 3] += dp[i - 1][smaller][three]
+
+    print(dp[l][True][True] + dp[l][False][True])
+
