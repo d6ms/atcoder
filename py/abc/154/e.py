d2af3eb9d8e362b95bad97d380462a154808dd49
d6ms <g.denden.ms@gmail.com>
2020-02-11 20:10:46 +0900

    abc 154 e

--- py/abc/154/e.py
+++ py/abc/154/e.py
@@ -3,18 +3,18 @@ def MI(): return map(int, input().split())
 def LMI(): return list(map(int, input().split()))
 
 
-# dp[i][j] := 上からi桁目までで0でない数字がちょうどj個ある数字の数 (i+1桁目以降は0)
-# dp[i][j] := dp[i - 1][j] + dp[i - 1][j - 1] * (D - 1)
-
+# dp[i + 1][smaller][j] = 上からi桁目までで0でない桁がj個ある数字の数
 N = input()
 K = I()
-dp = [[0] * (K + 1) for _ in range(len(N))]
-dp[0][1] = int(N[0])
+l = len(N)
+dp = [[[0] * (K + 1) for _ in range(2)] for _ in range(l + 1)]
+dp[0][False][0] = 1
+for i in range(1, l + 1):
+    N_i = int(N[i - 1])
+    for smaller in [True, False]:  # smaller := Nより小さいことが確定している
+        for j in range(K + 1):
+            for d in range((9 if smaller else N_i) + 1):
+                if j - (0 if d == 0 else 1) >= 0:
+                    dp[i][smaller or d < N_i][j] += dp[i - 1][smaller][j - (0 if d == 0 else 1)]
 
-beki = 10 ** (len(N) - 1)
-for i in range(1, len(N)):
-    beki = beki // 10
-    for j in range(K + 1):
-        dp[i][j] += dp[i - 1][j] + dp[i - 1][j - 1] * int(N[i]) * beki
-print(dp)
-print(dp[len(N) - 1][K])
+print(dp[l][True][K] + dp[l][False][K])
