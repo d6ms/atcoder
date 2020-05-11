24784fbaa9b69aae55005fc95df08e314b562747
d6ms <g.denden.ms@gmail.com>
2020-05-11 19:46:39 +0900

    doubling

--- py/lib/doubling.py
+++ py/lib/doubling.py
@@ -1,4 +1,4 @@
-from math import log2
+from math import log
 
 
 class Doubling(object):
@@ -11,9 +11,9 @@ class Doubling(object):
         :param next0: i番目の要素の1個先の番号を要素として持つリスト
                       次の要素が存在しない場合には-1を要素として保持する
         """
-        next = [[0] * N for _ in range(int(log2(K)) + 1)]
+        next = [[0] * N for _ in range(int(log(K, 2)) + 1)]
         next[0] = next0
-        for k in range(int(log2(K))):
+        for k in range(int(log(K, 2))):
             for i in range(N):
                 if next[k][i] == -1:
                     # 2^k個次に要素が無い時、当然2^(k+1)個次にも要素はない
@@ -27,7 +27,7 @@ class Doubling(object):
         """
         p番目の要素のK個次の要素を求める O(logK)
         """
-        for i in reversed(range(int(log2(K)) + 1)):
+        for i in reversed(range(int(log(K, 2)) + 1)):
             if p == -1:
                 return p
             if (K >> i) & 1:
