a904e8f719451208220c5523c6fc5f7767688bee
d6ms <g.denden.ms@gmail.com>
2020-05-12 20:19:28 +0900

    rolling hash

--- py/lib/string.py
+++ py/lib/string.py
@@ -1,3 +1,62 @@
+from random import randint
+
+
+class RollingHash(object):
+    mod = 1 << 61 - 1
+    base = randint(2, mod - 2)
+
+    def __init__(self, s):
+        """
+        sに関するローリングハッシュを構築する O(|s|)
+        cf. |s|<=5000 の構築で最大200ms程度
+        """
+        self.s = s
+        self.pw = pw = [1] * (len(s) + 1)
+
+        l = len(s)
+        self.h = h = [0] * (l + 1)
+
+        v = 0
+        for i in range(l):
+            h[i + 1] = v = (v * self.base + ord(s[i])) % self.mod
+        v = 1
+        for i in range(l):
+            pw[i + 1] = v = v * self.base % self.mod
+
+    def hash(self, l, r):
+        """ s[l:r] のhash (rはexclusive) O(1) """
+        return (self.h[r] - self.h[l] * self.pw[r - l]) % self.mod
+
+    def contains(self, t):
+        """
+        sがtを含むか O(|s|-|t|)
+        tはRollingHashオブジェクト
+        """
+        slen, tlen = len(self.s), len(t.s)
+        if slen < tlen:
+            return False
+        th = t.hash(0, tlen)
+        for i in range(slen - tlen + 1):
+            if th == self.hash(i, i + tlen):
+                return True
+        return False
+
+    def count(self, t):
+        """
+        s中に含まれるtの数をカウントする O(|s|-|t|)
+        tはRollingHashオブジェクト
+        """
+        slen, tlen = len(self.s), len(t.s)
+        if slen < tlen:
+            return 0
+        cnt = 0
+        th = t.hash(0, tlen)
+        for i in range(slen - tlen + 1):
+            if th == self.hash(i, i + tlen):
+                cnt += 1
+        return cnt
+
+
 def lcp_array(S):
     """
     LCP array (文字列の各インデックスについて「SとS[i: len(S) - 1]の最長共通接頭辞の長さ」を計算した配列)
